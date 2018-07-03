c767ce3d367a9e9a74a6a0666c71b59693b14652
Toby Inkster <mail@tobyinkster.co.uk>
2018-07-03 15:23:20 +0100

    Initial implementation, supports all of MooseX::Enumeration test suite, except metaobject stuff

---
+++ lib/MooX/Enumeration.pm
@@ -0,0 +1,241 @@
+use 5.010001;
+use strict;
+use warnings;
+no warnings 'once';
+
+package MooX::Enumeration;
+
+use Carp qw(croak);
+use Scalar::Util qw(blessed);
+use Sub::Util qw(set_subname);
+use B qw(perlstring);
+use namespace::autoclean;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.001';
+
+sub import {
+	my $class  = shift;
+	my $caller = caller;
+	$class->setup_for($caller);
+}
+
+sub setup_for {
+	my $class = shift;
+	my ($target) = @_;
+	
+	my $orig = $Moo::MAKERS{$target}{exports}{has}
+		or croak("$target doesn't have a `has` function");
+	
+	Moo::_install_tracked $target, has => sub {
+		if (@_ % 2 == 0) {
+			croak "Invalid options for attribute(s): even number of arguments expected, got " . scalar @_;
+		}
+		
+		my ($attrs, %spec) = @_;
+		$attrs = [$attrs] unless ref $attrs;
+		for my $attr (@$attrs) {
+			%spec = $class->process_spec($target, $attr, %spec);
+			if (ref $spec{handles}) {
+				my $handles = $spec{_orig_handles} = delete $spec{handles};
+				$class->install_delegates($target, $attr, $spec{isa}, %$handles);
+			}
+			$orig->($attr, %spec);
+		}
+		return;
+	};
+}
+
+sub process_spec {
+	my $class = shift;
+	my ($target, $attr, %spec) = @_;
+	
+	my @values;
+	
+	# Handle the type constraint stuff
+	if (exists $spec{isa} and exists $spec{enum}) {
+		croak "Cannot supply both the 'isa' and 'enum' options";
+	}
+	elsif (blessed $spec{isa} and $spec{isa}->isa('Type::Tiny::Enum')) {
+		@values = @{ $spec{isa}->values };
+	}
+	elsif (exists $spec{enum}) {
+		croak "Expected arrayref for enum" unless ref $spec{enum} eq 'ARRAY';
+		@values = @{ delete $spec{enum} };
+		require Type::Tiny::Enum;
+		$spec{isa} = Type::Tiny::Enum->new(values => \@values);
+	}
+	else {
+		# nothing to do
+		return %spec;
+	}
+	
+	# Canonicalize handles
+	if (my $handles = $spec{handles}) {
+		
+		if (!ref $handles and $handles eq 1) {
+			$handles = +{ map +( "is_$_" => [ "is", $_ ] ), @values };
+		}
+		
+		if (ref $handles eq 'ARRAY') {
+			$handles = +{ map ref($_)?@$_:($_=>[split/_/,$_,2]), @$handles };
+		}
+		
+		if (ref $handles eq 'HASH') {
+			for my $k (keys %$handles) {
+				next if ref $handles->{$k};
+				$handles->{$k}=[split/_/,$handles->{$k},2];
+			}
+		}
+		
+		$spec{handles} = $handles;
+	}
+	
+	# TODO: moosify stuff
+	
+	return %spec;
+}
+
+sub install_delegates {
+	my $class  = shift;
+	my ($target, $attr, $type, %delegates) = @_;
+	
+	for my $method (keys %delegates) {
+		my ($delegate_type, @delegate_params) = @{ $delegates{$method} };
+		my $builder = "build_${delegate_type}_delegate";
+		
+		no strict 'refs';
+		*{"${target}::${method}"} =
+			set_subname "${target}::${method}",
+			$class->$builder($target, $method, $attr, $type, @delegate_params);
+	}
+}
+
+sub build_is_delegate {
+	my $class  = shift;
+	my ($target, $method, $attr, $type, $match) = @_;
+	
+	if (ref $match) {
+		require match::simple;
+		return eval sprintf(
+			'sub { %s; match::simple::match($_[0]{%s}, $match) }',
+			$class->_build_throw_args($method, 0),
+			perlstring($attr),
+		);
+	}
+	elsif ($type->check($match)) {
+		return eval sprintf(
+			'sub { %s; $_[0]{%s} eq %s }',
+			$class->_build_throw_args($method, 0),
+			perlstring($attr),
+			perlstring($match),
+		);
+	}
+	else {
+		croak sprintf "Attribute $attr cannot be %s", perlstring($match);
+	}
+}
+
+sub build_assign_delegate {
+	my $class  = shift;
+	my ($target, $method, $attr, $type, $newvalue, $match) = @_;
+
+	croak sprintf "Attribute $attr cannot be %s", perlstring($newvalue)
+		unless $type->check($newvalue);
+	
+	my $err = 'Method %s cannot be called when attribute %s has value %s';
+
+	if (ref $match) {
+		require match::simple;
+		return eval sprintf(
+			'sub { %s; return $_[0] if $_[0]{%s} eq %s; match::simple::match($_[0]{%s}, $match) ? ($_[0]{%s}=%s) : Carp::croak(sprintf %s, %s, %s, $_[0]{%s}); $_[0] }',
+			$class->_build_throw_args($method, 0),
+			perlstring($attr),
+			perlstring($newvalue),
+			perlstring($attr),
+			perlstring($attr),
+			perlstring($newvalue),
+			perlstring($err),
+			perlstring($method),
+			perlstring($attr),
+			perlstring($attr),
+		);
+	}
+	elsif (defined $match) {
+		return eval sprintf(
+			'sub { %s; return $_[0] if $_[0]{%s} eq %s; ($_[0]{%s} eq %s) ? ($_[0]{%s}=%s) : Carp::croak(sprintf %s, %s, %s, $_[0]{%s}); $_[0] }',
+			$class->_build_throw_args($method, 0),
+			perlstring($attr),
+			perlstring($newvalue),
+			perlstring($attr),
+			perlstring($match),
+			perlstring($attr),
+			perlstring($newvalue),
+			perlstring($err),
+			perlstring($method),
+			perlstring($attr),
+			perlstring($attr),
+		);
+	}
+	else {
+		return eval sprintf(
+			'sub { %s; $_[0]{%s} = %s; $_[0] }',
+			$class->_build_throw_args($method, 0),
+			perlstring($attr),
+			perlstring($newvalue),
+		);
+	}
+}
+
+sub _build_throw_args {
+	my $class = shift;
+	my ($method, $n) = @_;
+	sprintf(
+		'Carp::croak(sprintf "Method %%s expects %%d arguments", %s, %d) if @_ != %d;',
+		perlstring($method),
+		$n,
+		$n+1,
+	);
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+MooX::Enumeration - shortcuts for working with enum attributes in Moo
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=MooX-Enumeration>.
+
+=head1 SEE ALSO
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2018 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
