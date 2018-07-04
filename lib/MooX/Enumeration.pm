1599371df9212ac44f5241ab3dcf1e500a9dfe06
Toby Inkster <mail@tobyinkster.co.uk>
2018-07-04 14:48:07 +0100

    bump version; changelog

--- lib/MooX/Enumeration.pm
+++ lib/MooX/Enumeration.pm
@@ -11,7 +11,7 @@ use Sub::Util qw(set_subname);
 use B qw(perlstring);
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.004';
+our $VERSION   = '0.005';
 
 sub import {
 	my $class  = shift;
